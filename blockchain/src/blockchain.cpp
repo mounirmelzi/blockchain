1c3ca84777168e75ed7f7db66edb44071657cd60
Mounir <lm_melzi@esi.dz>
2025-04-27 19:51:56 +0100

    Add project files.

---
+++ blockchain/src/blockchain.cpp
@@ -0,0 +1,12 @@
+﻿// blockchain.cpp : Defines the entry point for the application.
+//
+
+#include "blockchain.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
