48271c5d50679686980cae575a508f1921ed4427
Mounir <lm_melzi@esi.dz>
2025-05-03 01:59:15 +0100

    add rsa signature

--- blockchain/include/Utils.h
+++ blockchain/include/Utils.h
@@ -8,4 +8,6 @@ class Utils
 public:
 	static std::string hash(const std::string& data);
 	static std::pair<std::string, std::string> generateRSAKeyPair(unsigned int keySize = 1024);
+	static std::string sign(const std::string& privateKeyBase64, const std::string& data);
+	static bool verify(const std::string& publicKeyBase64, const std::string& data, const std::string& signatureBase64);
 };
--- blockchain/src/Utils.cpp
+++ blockchain/src/Utils.cpp
@@ -1,9 +1,12 @@
 #include "Utils.h"
+#include <string>
 #include <cryptopp/sha.h>
 #include <cryptopp/hex.h>
 #include <cryptopp/rsa.h>
 #include <cryptopp/osrng.h>
 #include "cryptopp/base64.h"
+#include "cryptopp/filters.h"
+#include "cryptopp/pssr.h"
 
 using namespace CryptoPP;
 
@@ -34,4 +37,41 @@ std::pair<std::string, std::string> Utils::generateRSAKeyPair(unsigned int keySi
     StringSource(privateKeyBin, true, new Base64Encoder(new StringSink(privateKeyBase64), false));
 
     return { publicKeyBase64, privateKeyBase64 };
+}
+
+std::string Utils::sign(const std::string& privateKeyBase64, const std::string& data) {
+    std::string privateKeyBin;
+    StringSource(privateKeyBase64, true, new Base64Decoder(new StringSink(privateKeyBin)));
+
+    RSA::PrivateKey privateKey;
+    StringSource privKeySource(privateKeyBin, true);
+    privateKey.Load(privKeySource);
+
+    AutoSeededRandomPool rng;
+    RSASS<PSS, SHA256>::Signer signer(privateKey);
+    std::string signature;
+    StringSource(data, true, new SignerFilter(rng, signer, new StringSink(signature)));
+
+    std::string encodedSignature;
+    StringSource(signature, true, new Base64Encoder(new StringSink(encodedSignature), false));
+
+    return encodedSignature;
+}
+
+bool Utils::verify(const std::string& publicKeyBase64, const std::string& data, const std::string& signatureBase64) {
+    std::string publicKeyBin;
+    StringSource(publicKeyBase64, true, new Base64Decoder(new StringSink(publicKeyBin)));
+   
+    RSA::PublicKey publicKey;
+    StringSource pubKeySource(publicKeyBin, true);
+    publicKey.Load(pubKeySource);
+
+    std::string signature;
+    StringSource(signatureBase64, true, new Base64Decoder(new StringSink(signature)));
+
+    RSASS<PSS, SHA256>::Verifier verifier(publicKey);
+    bool result = false;
+    StringSource(data + signature, true, new SignatureVerificationFilter(verifier, new ArraySink((byte*)&result, sizeof(result)), SignatureVerificationFilter::PUT_RESULT|SignatureVerificationFilter::SIGNATURE_AT_END));
+
+    return result;
 }
